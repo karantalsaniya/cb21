cb24a873d08da52fa186a6652d863d903b1a45c2
karantalsaniya <karantalsaniya@gmail.com>
2023-08-17 09:20:47 +0530

    a

---
+++ exercise/miliniumyear.c
@@ -0,0 +1,8 @@
+// write a prgoram to findout whether given year is milinium year or not.
+#include <stdio.h>
+void main()
+{
+    int year = 0;
+    printf("inter the year");
+    scanf("%d"&year);
+}
