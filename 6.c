1cf07c976afd3bd7f31b029d0d3fbfad4e2da263
karantalsaniya <karantalsaniya@gmail.com>
2023-08-20 14:28:29 +0530

    a

---
+++ 6.c
@@ -0,0 +1,22 @@
+// Write a C program to calculate final electricity bill based upon below given criteria. take monthly electricity unit from user as input.
+#include <stdio.h>
+void main()
+{
+    float a = 0, total_bill = 0, total_amount = 0; 
+
+    printf("Entre the Value of monthly electricity unit =");
+    scanf("%.2f",&a);
+
+    printf("Your monthly electricity unit is %0.2f",a);
+
+ 
+    //if (monthly_electricity_unit < 100)
+   // {
+        //total_bill_amount = monthly_electricity_unit * 1;
+        //total_amount = monthly_electricity_unit * 0.05;
+        //printf("\ntotal_bill_amount %.2f", total_bill_amount);
+       // printf("\ntotal_amount is %.2f", total_amount);
+ //   }
+
+    
+}
