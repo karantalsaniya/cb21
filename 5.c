c515a4f8af95a1294d2ebf4c39374c86fac2aa86
karantalsaniya <karantalsaniya@gmail.com>
2023-08-20 14:04:30 +0530

    a

--- 5.c
+++ 5.c
@@ -1,12 +1,45 @@
-// Write a programe to findout bmi category of user
+// Write a C program to calculate income tax, gross income, net income from monthly income given by user
 #include <stdio.h>
 void main()
 {
-    int weight = 0, feet = 0, inch = 0;
-    printf("Entre the Value of weight");
-    scnaf("%d", &weight);
-    printf("Entre the Value of feet");
-    scnaf("%d", &feet);
-    printf("Entre the Value of inch");
-    scnaf("%d",&inch);
+    float monthly_income = 0, Gross_income = 0, income_tax = 0, net_income = 0;
+
+    printf("Entre the Value of Monthly Income = ");
+    scanf("%f", &monthly_income);
+
+    Gross_income = (12.00 * monthly_income);
+
+    printf("\nGross income is %0.2f", Gross_income);
+
+    if (Gross_income < 300000)
+    {
+        income_tax = Gross_income * 0.05;
+        net_income = Gross_income - income_tax;
+        printf("\nIncome Tax is %.2f", income_tax);
+        printf("\nNet Income is %.2f", net_income);
+    }
+
+    else if (Gross_income >= 300000 && Gross_income < 500000)
+    {
+        income_tax = Gross_income * 0.1;
+        net_income = Gross_income - income_tax;
+        printf("\nIncome Tax is %.2f", income_tax);
+        printf("\nNet Income is %.2f", net_income);
+    }
+
+    else if (Gross_income >= 500000 && Gross_income < 800000)
+    {
+        income_tax = Gross_income * 0.2;
+        net_income = Gross_income - income_tax;
+        printf("\nIncome Tax is %.2f", income_tax);
+        printf("\nNet Income is %.2f", net_income);
+    }
+
+    else if (Gross_income >= 800000 )
+    {
+        income_tax = Gross_income * 0.3;
+        net_income = Gross_income - income_tax;
+        printf("\nIncome Tax is %.2f", income_tax);
+        printf("\nNet Income is %.2f", net_income);
+    }
 }
