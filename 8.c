5a57d10d187e5b297c320bd0cf40be8bc53c1edb
karantalsaniya <karantalsaniya@gmail.com>
2023-08-22 17:42:45 +0530

    a

---
+++ 8.c
@@ -0,0 +1,23 @@
+//Write a programe to print odd number till 3000
+#include<stdio.h>
+void main()
+{
+    int number=1 ;
+    printf("%d",number);
+    number=number+2 ;//3/
+while(number<3000)
+{
+    printf(" %d", number);
+    number=number+2 ;//3/
+}
+
+
+    // printf(%d,number);
+    // number=number+2 ;//5
+    // printf(%d,number);
+    // number=number+2 ;//7
+    // printf(%d,number);
+    // number=number+2 ;//9
+
+
+}
---
+++ 9.c
@@ -0,0 +1,34 @@
+// 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ... 1000
+#include<stdio.h>
+void main()
+{
+    int number = 0, ans=0;
+    
+    //printf("%d",ans);
+    //number=number+1;
+    //ans=ans+number;
+
+while (ans<1000)
+{
+   printf(" %d",ans);
+    number=number+1;
+    ans=ans+number;
+}
+
+
+
+    // printf(" %d",ans);
+    // number=number+1;
+    // ans=ans+number;
+    // printf(" %d",ans);
+    // number=number+1;
+    // ans=ans+number;
+    // printf(" %d",ans);
+    // number=number+1;
+    // ans=ans+number;
+    // printf(" %d",ans);
+    // number=number+1;
+    // ans=ans+number;
+
+
+}
